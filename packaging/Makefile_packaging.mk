2592b252fd28c4428695fc9b7d466fbabd8c7e48
Brian J. Murrell <brian.murrell@intel.com>
2021-04-28 11:38:19 -0400

    Add PR-repos-el8 support to packaging
    PR-repos-leap15: dpdk@PR-14:2
    PR-repos-el7: dpdk@PR-14:2
    PR-repos-el8: dpdk@PR-14:2
    Skip-PR-comments: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- packaging/Makefile_packaging.mk
+++ packaging/Makefile_packaging.mk
@@ -36,6 +36,7 @@ REPO_NAME ?= $(NAME)
 PR_REPOS                 ?= $(shell git show -s --format=%B | sed -ne 's/^PR-repos: *\(.*\)/\1/p')
 LEAP_15_PR_REPOS         ?= $(shell git show -s --format=%B | sed -ne 's/^PR-repos-leap15: *\(.*\)/\1/p')
 EL_7_PR_REPOS            ?= $(shell git show -s --format=%B | sed -ne 's/^PR-repos-el7: *\(.*\)/\1/p')
+EL_8_PR_REPOS            ?= $(shell git show -s --format=%B | sed -ne 's/^PR-repos-el8: *\(.*\)/\1/p')
 UBUNTU_20_04_PR_REPOS    ?= $(shell git show -s --format=%B | sed -ne 's/^PR-repos-ubuntu20: *\(.*\)/\1/p')
 
 COMMON_RPM_ARGS  := --define "_topdir $$PWD/_topdir" $(BUILD_DEFINES)
